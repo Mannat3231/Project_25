a9d7e27325e821395795834bd5d85814215c6689
Mannat3231 <101495379+Mannat3231@users.noreply.github.com>
2022-03-16 13:17:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -40,7 +40,7 @@ function keyPressed() {
 
 	if (keyCode === UP_ARROW){
 
-		Matter.Body.applyForce(paperObject.body,paperObject.Body.position,{x:130,y:-145});
+		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145});
 	}
 
 
